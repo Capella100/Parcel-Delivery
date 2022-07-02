e8209101a6deb122c088625daf76dad6642a6b59
capella100 <ugo1chiebuka@gmail.com>
2022-07-02 14:20:19 +0100

    add end point for featching all parcels

--- app.js
+++ app.js
@@ -7,9 +7,30 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+const parcels =[
+    {
+        id:1,
+        product: "phone",
+        description: "An Apple iPhone",
+        deliveryDate: "05/06/2022"
+    },
+    {
+        id:2,
+        product: "Laptop",
+        description: "An MacBook Air",
+        deliveryDate: "07/06/2022"
+    },
+    {
+        id:3,
+        product: "Bag",
+        description: "A Birkin Bag",
+        deliveryDate: "08/06/2022"
+    }
+]
+
+app.get('/parcels', (req, res) => {
     res.json({
-        message: "Hello World"
+       data:parcels
     })
 })
 
